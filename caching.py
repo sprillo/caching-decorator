81d517f73a8d561d37083c105ecdb2e1fa3000ee
Sebastian Prillo <10426884+sprillo@users.noreply.github.com>
2022-04-26 22:15:07 -0700

    Improve caching code by removing unused functionality and adding requested functionality.

--- caching.py
+++ caching.py
@@ -5,7 +5,7 @@ import pickle
 import sys
 from functools import wraps
 from inspect import signature
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 
 def init_logger():
@@ -23,7 +23,7 @@ init_logger()
 logger = logging.getLogger("caching")
 
 _CACHE_DIR = None
-_HASH = True
+_USE_HASH = False
 
 
 def set_cache_dir(cache_dir: str):
@@ -34,11 +34,6 @@ def set_cache_dir(cache_dir: str):
 
 def get_cache_dir():
     global _CACHE_DIR
-    if _CACHE_DIR is None:
-        raise Exception(
-            "Cache directory has not been set yet. "
-            "Please set it with set_cache_dir function."
-        )
     return _CACHE_DIR
 
 
@@ -47,15 +42,15 @@ def set_log_level(log_level: int):
     logger.setLevel(level=log_level)
 
 
-def set_hash(hash: bool):
-    logger.info(f"Setting cache to use hash: {hash}")
-    global _HASH
-    _HASH = hash
+def set_use_hash(use_hash: bool):
+    logger.info(f"Setting cache to use hash: {use_hash}")
+    global _USE_HASH
+    _USE_HASH = use_hash
 
 
-def get_hash():
-    global _HASH
-    return _HASH
+def get_use_hash():
+    global _USE_HASH
+    return _USE_HASH
 
 
 class CacheUsageError(Exception):
@@ -68,67 +63,77 @@ def hash_all(xs: List[str]) -> str:
 
 
 def cached(
-    cache_keys: Optional[Tuple] = None,
-    exclude_args: Optional[Tuple] = None,
+    exclude: Optional[List] = None,
+    exclude_if_default: Optional[List] = None,
 ):
     """
     Cache the wrapped function.
 
     The outputs of the wrapped function are cached by pickling them into a
     file whose path is determined by the function's name and it's arguments.
-    By default all arguments are used to define the cache key, but a custom
-    subset can be provided either via `cache_keys` by inclusion, or via
-    `exclude_args` by exclusion.
+    By default all arguments are used to define the cache key, but they can be
+    excluded via the `exclude` list. Moreover, an argument can be excluded
+    *only* if it has default values via the `exclude_if_default` list.
 
     Args:
-        cache_keys: What arguments to use for the hash key.
-        exclude_args: What arguments to exclude from the hash key. E.g.
+        exclude: What arguments to exclude from the hash key. E.g.
             n_processes, which does not affect the result of the function.
             If None, nothing will be excluded.
+        exclude_if_default: What arguments to exclude from the hash key *if
+            default*. E.g. new arguments introduced to a function as it is
+            extended with new functionality, while preserving the old
+            behavior when it has default values.
     """
 
     def f_res(func):
         @wraps(func)
         def r_res_res(*args, **kwargs):
-            # Get caching hyperparameters
             cache_dir = get_cache_dir()
-            hash = get_hash()
+            if cache_dir is None:
+                return func(*args, **kwargs)
+
+            use_hash = get_use_hash()
 
             s = signature(func)
-            binding = s.bind(*args, **kwargs)
-            binding.apply_defaults()
-            # Check that all cache_keys are present - user might have a typo!
-            if cache_keys is not None:
-                for cache_key in cache_keys:
-                    if cache_key not in binding.arguments:
-                        raise CacheUsageError(
-                            f"{cache_key} is not an argument to "
-                            "{func.__name__}. Fix the cache_keys."
-                        )
-            # Check that all exclude_args are present - user might have a typo!
-            if exclude_args is not None:
-                for arg in exclude_args:
-                    if arg not in binding.arguments:
+            # Check that all excluded args are present -
+            # user might have a typo!
+            if exclude is not None:
+                for arg in exclude:
+                    if arg not in s.parameters:
                         raise CacheUsageError(
                             f"{arg} is not an argument to {func.__name__}. "
-                            "Fix the exclude_args."
+                            "Fix the arguments in `exclude`."
                         )
-            # Only one of cache_keys and exclude_args can be provided
-            if cache_keys is not None and exclude_args is not None:
-                raise CacheUsageError(
-                    "Only one of cache_keys and exclude_args can be provided"
-                )
-            if not hash:
+            # Check that all exclude_if_default args are present -
+            # user might have a typo!
+            if exclude_if_default is not None:
+                for arg in exclude_if_default:
+                    if arg not in s.parameters:
+                        raise CacheUsageError(
+                            f"{arg} is not an argument to {func.__name__}. "
+                            "Fix the arguments in `exclude_if_default`."
+                        )
+
+            def excluded(arg, val) -> bool:
+                if exclude is not None and arg in exclude:
+                    return True
+                if exclude_if_default is not None and arg in exclude_if_default:
+                    p = s.parameters[arg]
+                    default = p.default
+                    if val == default:
+                        return True
+                return False
+
+            binding = s.bind(*args, **kwargs)
+            binding.apply_defaults()
+            if not use_hash:
                 path = (
                     [cache_dir]
                     + [f"{func.__name__}"]
                     + [
-                        f"{key}_{val}"
-                        for (key, val) in binding.arguments.items()
-                        if ((cache_keys is None) or (key in cache_keys))
-                        and (
-                            (exclude_args is None) or (key not in exclude_args)
-                        )
+                        f"{arg}_{val}"
+                        for (arg, val) in binding.arguments.items()
+                        if (not excluded(arg, val))
                     ]
                     + ["result"]
                 )
@@ -139,13 +144,9 @@ def cached(
                     + [
                         hash_all(
                             [
-                                f"{key}_{val}"
-                                for (key, val) in binding.arguments.items()
-                                if ((cache_keys is None) or (key in cache_keys))
-                                and (
-                                    (exclude_args is None)
-                                    or (key not in exclude_args)
-                                )
+                                f"{arg}_{val}"
+                                for (arg, val) in binding.arguments.items()
+                                if not excluded(arg, val)
                             ]
                         )
                     ]
